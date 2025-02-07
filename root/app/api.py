7c0d1a959274247d6fb9de9139278734e039299f
quietsy <github@qtsy.cc>
2025-02-07 22:07:39 +0200

    Add health endpoint

--- root/app/api.py
+++ root/app/api.py
@@ -1,17 +1,22 @@
 from flask import Flask
+from flask import jsonify
 from keyvaluestore import KeyValueStore
 
 api = Flask(__name__)
 
 
+@api.route('/health')
+def health():
+    return jsonify('Success')
+
 @api.route('/api/v1/images')
-def hello():
+def images():
     with KeyValueStore() as kv:
         return api.response_class(
-        response=kv['images'],
-        status=200,
-        mimetype='application/json'
-    )
+            response=kv['images'],
+            status=200,
+            mimetype='application/json'
+        )
 
 if __name__ == '__main__':
     api.run()
